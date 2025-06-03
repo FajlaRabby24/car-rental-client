b5521a21e42967ee1d034083aaa554ad4c3c22c0
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-06-03 20:07:57 +0600

    footer done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = () => {
       <header>
         <Navbar />
       </header>
-      <main>
+      <main className="min-h-[calc(100vh-385px)]">
         <Outlet />
       </main>
       <footer>
--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import { BsTwitterX } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io";
+import logo from "../assets/images/logo.png";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="bg-base-200">
+      <div className="footer footer-horizontal footer-center   p-10">
+        <aside>
+          <img src={logo} alt="logo" className="w-1/7" />
+          <p className="font-bold text-xl">Car Rental</p>
+          <p className="flex flex-col md:flex-row gap-3 font-semibold">
+            <span>Phone: +001258795</span>
+            <span>Email: car-rental@gmail.com</span>
+          </p>
+        </aside>
+        <nav>
+          <div className="grid grid-flow-col gap-4 ">
+            <button>
+              <FaFacebookF
+                className="p- bg-black cursor-pointer p-2 rounded-full "
+                color="#fff"
+                size={40}
+              />
+            </button>
+            <button>
+              <FaLinkedinIn
+                className="p- bg-black cursor-pointer p-2 rounded-full "
+                color="#fff"
+                size={40}
+              />
+            </button>
+            <button>
+              <BsTwitterX
+                className="p- bg-black cursor-pointer p-2 rounded-full "
+                color="#fff"
+                size={40}
+              />
+            </button>
+            <button>
+              <IoLogoYoutube
+                className="p- bg-black cursor-pointer p-2 rounded-full "
+                color="#fff"
+                size={40}
+              />
+            </button>
+          </div>
+        </nav>
+      </div>
+      <p className="text-center sm:font-normal py-2  ">
+        Copyright © {new Date().getFullYear()} - All right reserved by
+        car-rental.org
+      </p>
     </div>
   );
 };
