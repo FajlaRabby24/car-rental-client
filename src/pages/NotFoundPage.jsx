9e79304f11fdd3aee4f771e9b5bc70e4260c82ad
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-06-03 20:14:51 +0600

    added not found page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router";
+
+const NotFoundPage = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      }}
+    >
+      <div
+        className="hero-overlay "
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
+      ></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-4xl md:text-5xl font-bold">
+            Page Not Found!
+          </h1>
+          <p className="mb-5">
+            Oops! This recipe seems to have gone off the menu.
+          </p>
+          <Link to={"/"}>
+            <button className="btn btn-warning  md:btn-lg">
+              Go to Homepage
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
